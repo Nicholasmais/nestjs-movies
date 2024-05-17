6cb4149e9fadb05bdfcac17f0691c3f07607147d
Nicholasmais <nicoeras@hotmail.com>
2024-05-17 14:55:12 -0300

    read me no hello word

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,58 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `
+      <h1>API</h1>
+
+      <p>A API é uma plataforma para gerenciamento de usuários e filmes. Ela oferece endpoints para autenticação, criação de usuários, gerenciamento de sessões e manipulação de informações sobre filmes.</p>
+
+      <h2>Autenticação</h2>
+
+      <p>Para autenticação, a API utiliza tokens de acesso JWT (JSON Web Token). O token deve ser incluído no cabeçalho de autorização de todas as requisições autenticadas, no formato <code>Bearer &lt;token&gt;</code>.</p>
+
+      <h3>Registro de Usuário</h3>
+
+      <p>Endpoint: <code>POST /users</code></p>
+
+      <p>Para registrar um novo usuário, envie um JSON no corpo da requisição com os seguintes campos:</p>
+
+      <pre><code>{
+        "nome": "John",
+        "sobrenome": "Doe",
+        "type": "admin",
+        "status": "ativo",
+        "email": "nicholas2@example.com",
+        "senha": "123"
+      }</code></pre>
+
+      <h3>Login</h3>
+
+      <p>Endpoint: <code>POST /auth/login</code></p>
+
+      <p>Envie suas credenciais (email e senha) no corpo da requisição para obter um token de acesso.</p>
+
+      <h3>Logout</h3>
+
+      <p>Endpoint: <code>POST /auth/logout</code></p>
+
+      <p>Esta rota invalida o token atual, enviando-o para uma lista negra no Redis.</p>
+
+      <h2>Filmes</h2>
+
+      <p>A rota de filmes permite a manipulação de informações sobre filmes disponíveis.</p>
+
+      <h3>Adicionar Filme</h3>
+
+      <p>Endpoint: <code>POST /movies</code></p>
+
+      <p>Para adicionar um novo filme, envie um JSON no corpo da requisição com os seguintes campos:</p>
+
+      <pre><code>{
+        "nome": "Parasita",
+        "descricao": "Um filme sul-coreano dirigido por Bong Joon-ho, que aborda questões de desigualdade social por meio da história de uma família pobre que se infiltra em uma família rica.",
+        "genero": "Suspense / Drama",
+        "duracao": 132
+      }</code></pre>
+    `;
   }
 }
